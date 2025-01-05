ef18c6d74bb8c984336afaba8d8e6079f727b164
ALEKSANDREMG <159536199+ALEKSANDREMG@users.noreply.github.com>
2025-01-05 06:06:44 -0800

    Add files via upload

---
+++ Day 32/HW/hw.py
@@ -0,0 +1,5 @@
+my_list = [10, 20, 30, 40, 50]                                                                                                                                              
+print(my_list[0])
+
+my_list = [10, 20, 30, 40, 50]                                                                                                                                              
+print(my_list[-1])
