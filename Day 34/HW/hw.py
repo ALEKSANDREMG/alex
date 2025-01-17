e5fe64889c2b414befb3cacb48720c9c5a94efcf
ALEKSANDREMG <159536199+ALEKSANDREMG@users.noreply.github.com>
2025-01-17 06:57:48 -0800

    Add files via upload

---
+++ Day 34/HW/hw.py
@@ -0,0 +1,12 @@
+def sum_of_two_numbers(num1, num2):
+    print(num1 + num2)
+sum_of_two_numbers(5, 3)  
+
+def find_maximum(numbers):
+    print(max(numbers))
+numbers = [3, 5, 7, 2, 8]
+find_maximum(numbers) 
+
+def welcome_user(name):
+    print(f"Welcome {name}!")
+welcome_user("John")  
